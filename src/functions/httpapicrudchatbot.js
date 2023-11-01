0bd89978155bf0ed14249d3a944b1d453c26928a
robertoclunarc <robertoclunarg@gmail.com>
2023-11-01 01:18:11 -0400

    conecciones a la base de datos con get

--- src/functions/httpapicrudchatbot.js
+++ src/functions/httpapicrudchatbot.js
@@ -1,4 +1,5 @@
 const { app } = require('@azure/functions');
+const mysql = require('promise-mysql');
 
 app.http('httpapicrudchatbot', {
     methods: ['GET', 'POST'],
@@ -6,8 +7,66 @@ app.http('httpapicrudchatbot', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        //const name = request.query.get('name') || await request.text() || 'world';
+        const rutaAlArchivo = './DigiCertGlobalRootCA.crt(1).pem';
+        try {            
+        
+            const connection = await mysql.createPool({
+                host: 'serverchatbotmysql.mysql.database.azure.com',
+                user: 'adminchatbot',
+                password: 'd9AMmyVBS9fdXP',
+                database: 'azuredbchatbot',
+                ssl: {ca: `${rutaAlArchivo}`,
+                        rejectUnauthorized: false,
+                    },
+            });
 
-        return { body: `Hello, ${name}!` };
+            if (request.method === 'GET') {
+                // Manejar solicitud GET para obtener mensajes
+                const sender = request.query.get('sender');
+                const messages = await getMessagesForSender(connection, sender);
+                context.res = {
+                    body: JSON.stringify(messages),
+                };
+            } else if (request.method === 'POST') {
+                // Manejar solicitud POST para guardar mensajes
+                const requestData = request.body;
+                const chatId = await saveConversation(connection, requestData);
+                context.res = {
+                    body: { chatId },
+                };
+            } else {
+                context.res = {
+                    status: 405,
+                };
+            }
+
+            connection.end();
+        } catch (error) {
+            context.error(`Error en la conexión a la base de datos: ${error.message}`);
+            context.res = {
+                status: 500,
+                body: 'Error en la conexión a la base de datos',
+            };
+        }    
+
+        return context.res;
     }
 });
+
+async function getMessagesForSender(connection, sender) {
+    // Implementa la lógica para obtener mensajes de la base de datos
+    const result = await connection.query('SELECT a.*, b.* FROM chats a inner join azuredbchatbot.messages b on a.id=b.chat_id WHERE a.sender = ? order by b.id', [sender]);
+    return result;
+}
+
+async function saveConversation(connection, requestData) {
+    // Implementa la lógica para guardar la conversación en la base de datos
+    const [result] = await connection.execute('INSERT INTO chats (date, sender, object) VALUES (?, ?, ?)', [
+        requestData.date,
+        requestData.sender,
+        JSON.stringify(requestData),
+    ]);
+    const chatId = result[0].insertId;
+    return chatId;
+}
