cb2b4006301adb807da7dcd57ccdd078205954f8
robertoclunarc <robertoclunarg@gmail.com>
2023-11-19 19:54:15 -0400

    agregar get, post para users_pending_respond

--- src/functions/httpapicrudchatbot.js
+++ src/functions/httpapicrudchatbot.js
@@ -20,59 +20,79 @@ app.http('httpapicrudchatbot', {
             });
 
             if (request.method === 'GET') {
-                // Manejar solicitud GET para obtener mensajes
-                const sender = request.query.get('sender');
-                //context.log(request.query.getAll)
-                const messages = await getChatsWithMessages(connection,  sender);
+              // Manejar solicitud GET para obtener mensajes
+              const sender = request.query.get('sender');
+              if (sender) {
+                const messages = await getChatsWithMessages(connection, sender);
                 context.res = {
                     body: JSON.stringify(messages),
                 };
+              } else {
+                  const senderID = request.query.get('senderID');
+                  context.log(senderID);
+                  const usersPendingRespond = await queryDB(connection, 'SELECT * FROM users_pending_respond WHERE sender = ? and waiting=1', senderID);
+                  context.res = {
+                      body: JSON.stringify(usersPendingRespond),
+                  };
+              }  
             } else if (request.method === 'POST') {
-                    // Manejar solicitud POST para guardar mensajes
-                    try {
-                      const requestData = JSON.parse(await request.text());
-                  
-                      const values = [requestData.date, requestData.sender, requestData.object];
+                // Manejar solicitud POST para guardar mensajes
+                try {
+                  const requestData = JSON.parse(await request.text());
 
-                      let insertToDatabase;
-                      //const messagesExits = await getChatsWithMessages(connection, requestData.sender)
-                      const messagesExits = await queryDB(connection, 'SELECT * FROM chats WHERE sender = ? order by id', requestData.sender);
-                      
-                      if(messagesExits.length===0){
-                        context.log(`Mensaje: ${requestData.message}`);
-                        insertToDatabase = await saveChats(connection, values);
-                        context.log(insertToDatabase);
-                        const valuesMessages = [insertToDatabase?.insertId,requestData.role, requestData.message ];
-                        const insertMessage = await saveMessage(connection, valuesMessages);
-                        context.res = {
-                            body: {
-                                message:'Chat y Mensaje Registrado satisfactoriamente',
-                                idMessage: insertMessage.insertId
-                            }
-                        }
-                      }else{
-                        
-                        //const chat_id = await messagesExits.messages[0].conversation_history[0].chat;
-                        const chat_id = await messagesExits[0].id;
-                        const valuesMessages = [chat_id,requestData.role, requestData.message ];
-                        const insertMessage = await saveMessage(connection, valuesMessages);
-                        connection
-                        context.res = {
-                            body: {
-                                message:'Mensaje Registrado satisfactoriamente',
-                                idMessage: insertMessage.insertId
-                            }
-                        }
-                        context.log(messagesExits[0]);
-                      }
-                    } catch (error) {
-                      console.error('Error al procesar la solicitud:', error);
+                  if (requestData.waiting !== undefined) {
+                    // Nueva ruta para guardar usuarios pendientes de respuesta
+                    const result = await saveUserPendingRespond(connection, requestData);
+                    context.res = {
+                        body: {
+                            message: 'Usuario pendiente de respuesta registrado satisfactoriamente',
+                            id: result.insertId,
+                        },
+                    };
+                  } else {
+              
+                    const values = [requestData.date, requestData.sender, requestData.object];
+
+                    let insertToDatabase;
+                    //const messagesExits = await getChatsWithMessages(connection, requestData.sender)
+                    const messagesExits = await queryDB(connection, 'SELECT * FROM chats WHERE sender = ? order by id', requestData.sender);
+                    
+                    if(messagesExits.length===0){
+                      context.log(`Mensaje: ${requestData.message}`);
+                      insertToDatabase = await saveChats(connection, values);
+                      context.log(insertToDatabase);
+                      const valuesMessages = [insertToDatabase?.insertId,requestData.role, requestData.message ];
+                      const insertMessage = await saveMessage(connection, valuesMessages);
                       context.res = {
-                        status: 500,
-                        body: 'Error en el servidor'
-                      };
+                          body: {
+                              message:'Chat y Mensaje Registrado satisfactoriamente',
+                              idMessage: insertMessage.insertId
+                          }
+                      }
+                    }else{
+                      
+                      //const chat_id = await messagesExits.messages[0].conversation_history[0].chat;
+                      const chat_id = await messagesExits[0].id;
+                      const valuesMessages = [chat_id,requestData.role, requestData.message ];
+                      const insertMessage = await saveMessage(connection, valuesMessages);
+                      connection
+                      context.res = {
+                          body: {
+                              message:'Mensaje Registrado satisfactoriamente',
+                              idMessage: insertMessage.insertId
+                          }
+                      }
+                      context.log(messagesExits[0]);
                     }
-                  }
+                  }  
+                } catch (error) {
+                  console.error('Error al procesar la solicitud:', error);
+                  context.res = {
+                    status: 500,
+                    body: 'Error en el servidor'
+                  };
+                }
+              }
                   
             else {
                 context.res = {
@@ -132,34 +152,49 @@ async function getChatsWithMessages(connection, sender) {
 }
 
 const saveChats = (connection, values) => {
-    const insertQuery = 'INSERT INTO chats (date, sender, object) VALUES (?, ?, ?)';
-    return new Promise((resolve, reject) => {
-      connection.query(insertQuery, values, (err, results) => {
-        if (err) {
-          console.error('Error al ejecutar la consulta SQL:', err);
-          reject(err);
-        } else {
-          console.log('Chats insertados con éxito:');
-          resolve(results);
-        }
-      });
+  const insertQuery = 'INSERT INTO chats (date, sender, object) VALUES (?, ?, ?)';
+  return new Promise((resolve, reject) => {
+    connection.query(insertQuery, values, (err, results) => {
+      if (err) {
+        console.error('Error al ejecutar la consulta SQL:', err);
+        reject(err);
+      } else {
+        console.log('Chats insertados con éxito:');
+        resolve(results);
+      }
     });
-  };
-  const saveMessage = (connection, values) => {
-    const insertQuery = 'INSERT INTO messages (chat_id, role, content) VALUES (?, ?, ?)';
-    return new Promise((resolve, reject) => {
-      connection.query(insertQuery, values, (err, results) => {
-        if (err) {
-          console.error('Error al ejecutar la consulta SQL:', err);
-          reject(err);
-        } else {
-          console.log('Mensajes insertados con éxito:');
-          resolve(results);
-        }
-      });
+  });
+};
+
+const saveMessage = (connection, values) => {
+  const insertQuery = 'INSERT INTO messages (chat_id, role, content) VALUES (?, ?, ?)';
+  return new Promise((resolve, reject) => {
+    connection.query(insertQuery, values, (err, results) => {
+      if (err) {
+        console.error('Error al ejecutar la consulta SQL:', err);
+        reject(err);
+      } else {
+        console.log('Mensajes insertados con éxito:');
+        resolve(results);
+      }
     });
-  };
-  
+  });
+};
+
+async function saveUserPendingRespond(connection, values) {
+  const insertQuery = 'INSERT INTO users_pending_respond (sender, waiting) VALUES (?, ?)';
+  return new Promise((resolve, reject) => {
+      connection.query(insertQuery, [values.sender, values.waiting], (err, results) => {
+          if (err) {
+              console.error('Error al ejecutar la consulta SQL:', err);
+              reject(err);
+          } else {
+              console.log('Usuario pendiente de respuesta insertado con éxito:');
+              resolve(results);
+          }
+      });
+  });
+}
 
 async function queryDB(connection, query, fields = undefined) {
     let result = [];
